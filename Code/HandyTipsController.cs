6d5eb0e1048576f092c7134ac399d404fd272c3d
Bogdan Litescu <bogdan.litescu@gmail.com>
2013-11-03 23:07:15 +0800

    prepared 1.5 install package (http://dl.dnnsharp.com/HTIPS/DnnSharp.HandyTips.1.5.zip)

--- Code/HandyTipsController.cs
+++ Code/HandyTipsController.cs
@@ -1,74 +1,12 @@
-
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Xml;
-using System.Web;
-using System.Web.Configuration;
-using System.Net;
-using System.Web.UI.WebControls;
-using System.Xml.Xsl;
-using System.Xml.XPath;
-using System.Text;
-using Microsoft.ApplicationBlocks.Data;
-using System.Security.Cryptography;
-
-using DotNetNuke;
-using DotNetNuke.Common;
-using DotNetNuke.Common.Lists;
-using DotNetNuke.Common.Utilities;
-using DotNetNuke.Entities.Tabs;
-using DotNetNuke.Entities.Modules;
-using DotNetNuke.Entities.Portals;
-using DotNetNuke.Services.Search;
-using DotNetNuke.Entities.Users;
-using DotNetNuke.Security;
-using DotNetNuke.Security.Permissions;
-using DotNetNuke.Security.Roles;
-using System.IO;
-using System.Text.RegularExpressions;
-
 
 namespace avt.HandyTips
 {
     public class HandyTipsController
     {
-        private string _objectQualifier;
-        private string _databaseOwner;
-
-        static public string ProductCode = "HTIPS";
-        static public string Version = "1.4";
-        static public string VersionAll = "1.4.0";
-
         public HandyTipsController()
         {
-            // Read the configuration specific information for this provider
-            DotNetNuke.Framework.Providers.ProviderConfiguration _providerConfiguration = DotNetNuke.Framework.Providers.ProviderConfiguration.GetProviderConfiguration("data");
-            DotNetNuke.Framework.Providers.Provider objProvider = (DotNetNuke.Framework.Providers.Provider)_providerConfiguration.Providers[_providerConfiguration.DefaultProvider];
-
-            // Read the attributes for this provider
-            //Get Connection string from web.config
-            string _connectionString = DotNetNuke.Common.Utilities.Config.GetConnectionString();
-
-            if (_connectionString == "") {
-                // Use connection string specified in provider
-                _connectionString = objProvider.Attributes["connectionString"];
-            }
-
-            string _providerPath = objProvider.Attributes["providerPath"];
-
-            _objectQualifier = objProvider.Attributes["objectQualifier"];
-            if (_objectQualifier != "" & _objectQualifier.EndsWith("_") == false) {
-                _objectQualifier += "_";
-            }
-
-            _databaseOwner = objProvider.Attributes["databaseOwner"];
-            if (_databaseOwner != "" & _databaseOwner.EndsWith(".") == false) {
-                _databaseOwner += ".";
-            }
+            
         }
 
     }
